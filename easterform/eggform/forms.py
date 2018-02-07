3c224b19cf7715163a048deccbbd03b5d29fd4cb
DrvTovar <roseavenilia@gmail.com>
2018-02-07 15:57:41 -0600

    update to the index.html

--- easterform/eggform/forms.py
+++ easterform/eggform/forms.py
@@ -4,7 +4,7 @@ from eggform.models import Eggs
 eggCount = [('50',50),('100',100)]
 
 class EggForm(forms.ModelForm):
-    eggs = forms.ChoiceField(choices=eggCount, widget=forms.RadioSelect())
+    eggs = forms.ChoiceField(choices=eggCount, widget=forms.RadioSelect(),label='Number of Eggs?')
     payEmail = forms.EmailField(label = 'PayPal Email')
     number = forms.CharField(label = 'Phone Number')
     class Meta:
